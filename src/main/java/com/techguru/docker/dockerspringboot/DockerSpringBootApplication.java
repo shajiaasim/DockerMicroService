fbf70289dcd260a440d799e1f056ad39375caa75
Aasim Shaji <aasimshaji@Aasims-iMac.local>
2018-05-06 10:49:43 -0500

    Initial commit

---
+++ src/main/java/com/techguru/docker/dockerspringboot/DockerSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.techguru.docker.dockerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootApplication.class, args);
+	}
+}
---
+++ src/main/java/com/techguru/docker/dockerspringboot/resource/HelloResource.java
@@ -0,0 +1,18 @@
+package com.techguru.docker.dockerspringboot.resource;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rest/docker/hello")
+public class HelloResource {
+
+    @GetMapping
+    public String hello(){
+
+        return "Hello";
+    }
+
+}
